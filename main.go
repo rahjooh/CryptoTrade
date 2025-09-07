d3cd58b328ce74cdf240b61b33bb3e9d5f7ae736
Hadi Rahjou <rahjooh@gmail.com>
2025-09-07 13:03:43 +0000

    feat: add cloudwatch

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	logger.InitCloudWatch(cfg.Storage.S3.Region, cfg.Cryptoflow.Name)
+	logger.InitCloudWatch(cfg.Storage.S3.Region, cfg.Cryptoflow.Name, cfg.Logging.DashboardName)
 
 	if strings.ToLower(cfg.Logging.Level) == "report" {
 		logger.StartReport(ctx, log, 30*time.Second)
