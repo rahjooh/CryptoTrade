4e3be03f071f358ac70724ea20bb35cbf503a08d
Hadi Rahjou <rahjooh@gmail.com>
2025-09-07 09:52:55 +0000

    feat: add cloudwatch

--- logger/cloudwatch.go
+++ logger/cloudwatch.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -38,5 +39,37 @@ func publishMetrics(ctx context.Context, data []cwtypes.MetricDatum) {
 	_, _ = cwClient.PutMetricData(ctx, &cloudwatch.PutMetricDataInput{
 		Namespace:  aws.String(cwNamespace),
 		MetricData: data,
+	})
+}
+
+// CreateDefaultDashboard creates or updates a basic CloudWatch dashboard with
+// common system metrics. If the CloudWatch client is not configured, the
+// function exits without performing any action.
+func CreateDefaultDashboard(ctx context.Context) {
+	if cwClient == nil {
+		return
+	}
+
+	body := fmt.Sprintf(`{
+"widgets": [{
+"type": "metric",
+"width": 24,
+"height": 6,
+"properties": {
+"metrics": [
+    ["%[1]s","Hadi-CPUPercent"],
+    ["%[1]s","Hadi-MemoryMB"],
+    ["%[1]s","Hadi-DiskMB"]
+],
+"period": 60,
+"stat": "Average",
+"title": "CryptoFlow System Metrics"
+}
+}]
+}`, cwNamespace)
+
+	_, _ = cwClient.PutDashboard(ctx, &cloudwatch.PutDashboardInput{
+		DashboardName: aws.String(cwNamespace + "-dashboard"),
+		DashboardBody: aws.String(body),
 	})
 }
