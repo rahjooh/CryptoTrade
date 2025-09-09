5b7dc8d0e87542ab1f4d894946c82a8f01f371d5
Hadi Rahjou <rahjooh@gmail.com>
2025-09-09 09:29:13 +0000

    FIX: correct reader/okx/fobd

--- reader/okx/fobd.go
+++ reader/okx/fobd.go
@@ -130,7 +130,11 @@ func (r *Okx_FOBD_Reader) stream(symbols []string, wsURL string) {
 
 		args := make([]map[string]string, 0, len(symbols))
 		for _, sym := range symbols {
-			args = append(args, map[string]string{"channel": "books-l2-tbt", "instId": sym})
+			args = append(args, map[string]string{
+				"channel":  "books-l2-tbt",
+				"instType": "SWAP",
+				"instId":   sym,
+			})
 		}
 		sub := map[string]interface{}{"op": "subscribe", "args": args}
 		if err := conn.WriteJSON(sub); err != nil {
